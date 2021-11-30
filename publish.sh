db7fd7fd8de1aa6d61584483297558ba18544e40
tangyj <you@example.com>
2021-11-30 11:28:05 +0800

    2021-11-30 11:28:00

--- publish.sh
+++ publish.sh
@@ -5,4 +5,4 @@ if test $? = 0 ; then
     export ALL_PROXY=socks5://127.0.0.1:1086
 fi
 
-/usr/bin/git pull && echo "git pull ok\n" && hugo -D && echo "compile ok\n" && /usr/bin/git add . && echo "git add ok\n" && /usr/bin/git commit -m "$date" && echo "git commit ok\n"   && /usr/bin/git push && echo "git push ok\n" && git subtree pull --prefix public origin gh-pages  echo "git subtree push ok\n"&& git subtree push --prefix public origin gh-pages && echo "git subtree push ok" 
+/usr/bin/git pull && echo "git pull ok\n" && hugo -D && echo "compile ok\n" && /usr/bin/git add . && echo "git add ok\n" && /usr/bin/git commit -m "$date" && echo "git commit ok\n"   && /usr/bin/git push && echo "git push ok\n" && git subtree pull --prefix public origin gh-pages && echo "git subtree push ok\n"&& git subtree push --prefix public origin gh-pages && echo "git subtree push ok" 
