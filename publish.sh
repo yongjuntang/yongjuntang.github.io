5c375d1d8f8fa4e7a92909a5a2dd64cfa6c7a9be
tangyj <you@example.com>
2021-11-30 10:32:38 +0800

    2021-11-30 10:32:37

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 date=$(date "+%Y-%m-%d %H:%M:%S")
-hugo -D && /usr/bin/git add . && /usr/bin/git commit -m "$date" && /usr/bin/git push && git subtree push --prefix public origin ph-pages && echo "ok"
+hugo -D && /usr/bin/git add . && /usr/bin/git commit -m "$date" && /usr/bin/git push && git subtree push --prefix public origin gh-pages && echo "ok"
