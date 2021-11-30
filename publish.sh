c3a814723f8e74589b8fdd7cc07bb0722e49a7d6
tangyj <you@example.com>
2021-11-30 11:16:49 +0800

    2021-11-30 11:16:48

--- publish.sh
+++ publish.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 date=$(date "+%Y-%m-%d %H:%M:%S")
+alias proxy="ALL_PROXY=socks5://127.0.0.1:1086"
+proxy /usr/bin/git pull
+echo $?
+if $? == 0 ; then
+    export ALL_PROXY=socks5://127.0.0.1:1086
+fi
+
 hugo -D && /usr/bin/git add . && /usr/bin/git commit -m "$date" && /usr/bin/git pull && echo "0\n" && /usr/bin/git push && echo "1\n"&& git subtree push --prefix public origin gh-pages ; echo "ok"
